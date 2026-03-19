dae260f3577c37e51a7f15dc6fc43b990607952b
Lucas Silveira <lucas.a.m.silveira30@gmail.com>
2026-03-19 13:09:16 -0300

    Movendo projeto para o repositório remoto

---
+++ src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -0,0 +1,36 @@
+package br.com.alura.minhasmusicas.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalDeReproducoes;
+    private int totalDeCurtidas;
+    private int classificacao;
+
+    public void curtir() {
+        this.totalDeCurtidas++;
+    }
+
+    public void reproduzir() {
+        this.totalDeReproducoes++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalDeReproducoes() {
+        return totalDeReproducoes;
+    }
+
+    public int getTotalDeCurtidas() {
+        return totalDeCurtidas;
+    }
+
+    public int getClassificacao() {
+        return classificacao;
+    }
+}
